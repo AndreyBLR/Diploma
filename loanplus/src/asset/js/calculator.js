3d34fbf1813c474d507806fc7f091520d970517a
Andrey <andreykukalev@gmail.com>
2019-05-09 19:02:05 +0300

    extended calculator is added

--- loanplus/src/asset/js/calculator.js
+++ loanplus/src/asset/js/calculator.js
@@ -109,17 +109,49 @@ function loan_calculate(active_option) {
     var emi = ((loanAmount * monthlyInterestRatio) * sp);
     var full = numberOfMonths * emi;
     var interest = full - loanAmount;
-    var int_pge = (interest / full) * 100;
-    //$("#tbl_int_pge").html(int_pge.toFixed(2)+" %");
-    //$("#tbl_loan_pge").html((100-int_pge.toFixed(2))+" %");
 
     var emi_str = emi.toFixed(0).toString().replace(/,/g, "").replace(/\B(?=(\d{3})+(?!\d))/g, " ");
-    var loanAmount_str = loanAmount.toString().replace(/,/g, "").replace(/\B(?=(\d{3})+(?!\d))/g, " ");
     var full_str = full.toFixed(0).toString().replace(/,/g, "").replace(/\B(?=(\d{3})+(?!\d))/g, " ");
     var int_str = interest.toFixed(0).toString().replace(/,/g, "").replace(/\B(?=(\d{3})+(?!\d))/g, " ");
-
 
     $("#emi").html(emi_str + " " + active_option.value);
     $("#tbl_emi").html(int_str + " " + active_option.value);
     $("#tbl_la").html(full_str + " " + active_option.value);
+
+    var loantable = $("#loantable");
+
+    if (loantable.length) {
+        var detailDesc = "<thead><tr class='table-head'><th>Payment No.</th><th>Begining Balance</th><th>EMI</th><th>Principal</th><th>Interest</th><th>Ending Balance</th></thead><tbody>";
+        var bb = parseInt(loanAmount);
+        var int_dd = 0;
+        var pre_dd = 0;
+        var end_dd = 0;
+        for (var j = 1; j <= numberOfMonths; j++) {
+            int_dd = bb * (rateOfInterest / 100 / 12);
+            pre_dd = emi.toFixed(2) - int_dd.toFixed(2);
+            end_dd = bb - pre_dd.toFixed(2);
+            detailDesc +=
+                "<tr><td>" +
+                j +
+                "</td><td>" +
+                bb.toFixed(2) +
+                "</td><td>" +
+                emi.toFixed(2) +
+                "</td><td>" +
+                pre_dd.toFixed(2) +
+                "</td><td>" +
+                int_dd.toFixed(2) +
+                "</td><td>" +
+                end_dd.toFixed(2) +
+                "</td></tr>";
+            bb = bb - pre_dd.toFixed(2);
+        }
+        detailDesc += "</tbody>";
+        loantable.html(detailDesc);
+    }
+
+
+
+
+
 }

306f8d62f38c9b7387b5397ccf445e7dc23ccae0
Andrey <andreykukalev@gmail.com>
2019-05-09 17:22:59 +0300

    calculator is updated to show currency label near to values in calculation frame

--- loanplus/src/asset/js/calculator.js
+++ loanplus/src/asset/js/calculator.js
@@ -1,6 +1,6 @@
-(function ($) {
+const select_element = document.querySelector('#currency-select');
 
-    const select_element = document.querySelector('#currency-select');
+(function ($) {
 
     select_element.addEventListener('change', (event) => {
 
@@ -14,6 +14,14 @@
         $("#pricipal-slide").slider("option", "max", max);
         $("#pricipal").text(min);
 
+        min = parseFloat(active_option.getAttribute("min-month"));
+        max = parseFloat(active_option.getAttribute("max-month"));
+
+        $("#totalyear-slide").slider("option", "min", min);
+        $("#totalyear-slide").slider("option", "value", min);
+        $("#totalyear-slide").slider("option", "max", max);
+        $("#totalyear").text(min);
+
         min = parseFloat(active_option.getAttribute("min-interest"));
         max = parseFloat(active_option.getAttribute("max-interest"));
 
@@ -24,65 +32,71 @@
 
         $("#currency-span").text(event.currentTarget.value);
 
-        loancalculate();
+        loan_calculate(event.currentTarget);
     });
 
-    var active_option = select_element.options[select_element.selectedIndex];
+    active_option = select_element.options[select_element.selectedIndex];
+
+    initialize_sliders(active_option);
+    loan_calculate(active_option);
+
+})(jQuery);
+
+function initialize_sliders(active_option) {
 
     $("#currency-span").text(active_option.value);
 
-    var min = parseFloat(active_option.getAttribute("min-principal"));
-    var max = parseFloat(active_option.getAttribute("max-principal"));
+    var min_value = parseFloat(active_option.getAttribute("min-principal"));
+    var max_value = parseFloat(active_option.getAttribute("max-principal"));
 
     $("#pricipal-slide").slider({
         range: "min",
-        min: min,
-        max: max,
-        value: min,
+        min: min_value,
+        max: max_value,
+        value: min_value,
         step: 1000,
         slide: function (event, ui) {
             $("#pricipal").text(ui.value);
-            loancalculate();
+            loan_calculate(select_element.options[select_element.selectedIndex]);
         }
     });
     $("#pricipal").text($("#pricipal-slide").slider("value"));
 
-    min = parseFloat(active_option.getAttribute("min-month"));
-    max = parseFloat(active_option.getAttribute("max-month"));
+    min_value = parseFloat(active_option.getAttribute("min-month"));
+    max_value = parseFloat(active_option.getAttribute("max-month"));
 
     $("#totalyear-slide").slider({
         range: "min",
-        min: min,
-        max: max,
+        min: min_value,
+        max: max_value,
         step: 3,
-        value: min,
+        value: min_value,
         slide: function (event, ui) {
             $("#totalyear").text(ui.value);
-            loancalculate();
+            loan_calculate(select_element.options[select_element.selectedIndex]);
         }
     });
     $("#totalyear").text($("#totalyear-slide").slider("value"));
 
-    min = parseFloat(active_option.getAttribute("min-interest"));
-    max = parseFloat(active_option.getAttribute("max-interest"));
+    min_value = parseFloat(active_option.getAttribute("min-interest"));
+    max_value = parseFloat(active_option.getAttribute("max-interest"));
 
     $("#intrest-slide").slider({
         range: "min",
-        min: min,
-        max: max,
+        min: min_value,
+        max: max_value,
         step: 0.1,
-        value: min,
+        value: min_value,
         slide: function (event, ui) {
             $("#intrest").text(ui.value);
-            loancalculate();
+            loan_calculate(select_element.options[select_element.selectedIndex]);
         }
     });
     $("#intrest").text($("#intrest-slide").slider("value"));
+}
 
-    loancalculate();
-})(jQuery);
 
-function loancalculate() {
+function loan_calculate(active_option) {
     var loanAmount = $("#pricipal").text();
     var numberOfMonths = $("#totalyear").text();
     var rateOfInterest = $("#intrest").text();
@@ -105,7 +119,7 @@ function loancalculate() {
     var int_str = interest.toFixed(0).toString().replace(/,/g, "").replace(/\B(?=(\d{3})+(?!\d))/g, " ");
 
 
-    $("#emi").html(emi_str);
-    $("#tbl_emi").html(int_str);
-    $("#tbl_la").html(full_str);
+    $("#emi").html(emi_str + " " + active_option.value);
+    $("#tbl_emi").html(int_str + " " + active_option.value);
+    $("#tbl_la").html(full_str + " " + active_option.value);
 }

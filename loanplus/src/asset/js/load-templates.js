b4b658bbf03bde09174b75dd21873425ce143a95
enprecis\akukalev <akukalev@enprecis.com>
2019-05-10 19:42:02 +0300

    Template for request form is added

--- loanplus/src/asset/js/load-templates.js
+++ loanplus/src/asset/js/load-templates.js
@@ -1,25 +1,44 @@
 $(function () {
     var base_url = window.location.protocol + "//" + window.location.host + "//";
 
-    $.ajax({
-        url: base_url + 'templates/header.html',
-        async: false,
-        success: function (content) {
-            $('#header').append(content);
-        },
-        error: function (jqXHR, textStatus, errorThrown) {
-            console.log('error');
-        }
-    });
+    if ($('#header').length) {
+        $.ajax({
+            url: base_url + 'templates/header.html',
+            async: false,
+            success: function (content) {
+                $('#header').append(content);
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                console.log('error');
+            }
+        });
+    }
 
-    $.ajax({
-        url: base_url + 'templates/footer.html',
-        async: false,
-        success: function (content) {
-            $('#footer').append(content);
-        },
-        error: function (jqXHR, textStatus, errorThrown) {
-            console.log('error');
-        }
-    });
-});         
+    if ($('#footer').length) {
+        $.ajax({
+            url: base_url + 'templates/footer.html',
+            async: false,
+            success: function (content) {
+                $('#footer').append(content);
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                console.log('error');
+            }
+        });
+    }
+
+    if ($('#request-form').length) {
+        $.ajax({
+            url: base_url + 'templates/request_form.html',
+            async: false,
+            success: function (content) {
+                $('#request-form').append(content);
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                console.log('error');
+            }
+        });
+    }
+});
+
+

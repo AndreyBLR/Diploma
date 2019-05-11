59da678bf2f6faa08a87d144483872eeb293f731
enprecis\akukalev <akukalev@enprecis.com>
2019-05-11 17:37:50 +0300

    SendEmailApi project is added

---
+++ SendEmailApi/Models/Email.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SendEmailApi.Models
+{
+    public class Email
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int Sum { get; set; }
+        public string Text { get; set; }
+    }
+}

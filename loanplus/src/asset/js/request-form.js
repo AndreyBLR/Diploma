f6078ad27608bf9f9d6a9810c5fefc64ebf6b2f4
Andrey <andreykukalev@gmail.com>
2019-05-13 01:36:50 +0300

    Remove unused pages

--- loanplus/src/asset/js/request-form.js
+++ loanplus/src/asset/js/request-form.js
@@ -6,7 +6,14 @@ $(function () {
         var phone = $("#phone-request")[0].value;
         var text = $("#text-request")[0].value;
 
-        console.log(sum);
+        var emailBody = 'Имя: ' + name + '%0A'; //%0A - символ новой строки
+        emailBody += 'Сумма: ' + sum + '%0A';
+        emailBody += 'Email: ' + email + '%0A';
+        emailBody += 'Тел: ' + phone + '%0A%0A%0A';
+        emailBody += 'Доп. информация: ' + text;
+
+
+        window.location.assign('mailto:support@ideabank.com?subject=Заявка&body=' + emailBody);
         e.preventDefault();
     });
 })
